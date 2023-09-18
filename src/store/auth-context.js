fdb9213bebca50631f5f10b03858c5b2133aad50
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2023-09-18 10:29:15 +0000

    making context dynamic

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+  isLoggedIn: false,
+  isLooggedOut: false,
 });
 
 export default AuthContext;
